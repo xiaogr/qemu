ad3a58d84159a5894f29ba676eec3ec836acc10f
Xiao Guangrong <guangrong.xiao@linux.intel.com>
2015-09-11 11:26:24 +0800

    hostmem-file: clean up memory allocation
    - hostmem-file.c is compiled only if CONFIG_LINUX is enabled so that is
      unnecessary to do the same check in the source file
    
    - the interface, HostMemoryBackendClass->alloc(), is not called many
      times, do not need to check if the memory-region is initialized
    
    Signed-off-by: Xiao Guangrong <guangrong.xiao@linux.intel.com>

--- backends/hostmem-file.c
+++ backends/hostmem-file.c
@@ -46,17 +46,12 @@ file_backend_memory_alloc(HostMemoryBackend *backend, Error **errp)
         error_setg(errp, "mem-path property not set");
         return;
     }
-#ifndef CONFIG_LINUX
-    error_setg(errp, "-mem-path not supported on this host");
-#else
-    if (!memory_region_size(&backend->mr)) {
-        backend->force_prealloc = mem_prealloc;
-        memory_region_init_ram_from_file(&backend->mr, OBJECT(backend),
+
+    backend->force_prealloc = mem_prealloc;
+    memory_region_init_ram_from_file(&backend->mr, OBJECT(backend),
                                  object_get_canonical_path(OBJECT(backend)),
                                  backend->size, fb->share,
                                  fb->mem_path, errp);
-    }
-#endif
 }
 
 static void
