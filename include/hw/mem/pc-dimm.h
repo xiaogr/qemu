1eed6181f4cb857561e967859beefbd7475d2298
Xiao Guangrong <guangrong.xiao@linux.intel.com>
2015-10-06 18:13:38 +0800

    pc-dimm: make pc_existing_dimms_capacity static and rename it
    pc_existing_dimms_capacity() can be static since it is not used out of
    pc-dimm.c and drop the pc_ prefix to prepare the work which abstracts
    dimm device type from pc-dimm
    
    Signed-off-by: Xiao Guangrong <guangrong.xiao@linux.intel.com>

--- include/hw/mem/pc-dimm.h
+++ include/hw/mem/pc-dimm.h
@@ -87,7 +87,6 @@ uint64_t pc_dimm_get_free_addr(uint64_t address_space_start,
 int pc_dimm_get_free_slot(const int *hint, int max_slots, Error **errp);
 
 int qmp_pc_dimm_device_list(Object *obj, void *opaque);
-uint64_t pc_existing_dimms_capacity(Error **errp);
 void pc_dimm_memory_plug(DeviceState *dev, MemoryHotplugState *hpms,
                          MemoryRegion *mr, uint64_t align, bool gap,
                          Error **errp);
